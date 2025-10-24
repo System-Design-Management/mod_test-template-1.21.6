ae4f5f3a71ff0c54cc9f9e24807b92af27c7fdcd
miyoshi207 <miyoshi-naoki207@g.ecc.u-tokyo.ac.jp>
2025-10-24 11:53:36 +0900

    パッケージに分割

--- src/client/java/com/example/Mod_testClient.java
+++ src/client/java/com/example/Mod_testClient.java
@@ -1,13 +1,10 @@
 package com.example;
 
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.minecraft.text.MutableText;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
+import com.example.event.ClientJoinHandler;
 
 public class Mod_testClient implements ClientModInitializer {
 
@@ -15,33 +12,8 @@ public class Mod_testClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-        LOGGER.info("[ModTest] onInitializeClient is CALLED.");
+        LOGGER.info("[ModTest] Initializing client...");
 
-        // クライアントがワールドに参加した時のイベントを登録
-        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
-
-            // ワールドに入った瞬間にメインスレッドで実行
-            client.execute(() -> {
-                LOGGER.info("[ModTest] Client joined world! Showing title.");
-
-                // 単位はTick。20 Ticks = 1秒
-                client.inGameHud.setTitleTicks(10, 40, 10);
-
-                // メインタイトルを設定
-                MutableText mainText = Text.literal("これが");
-                MutableText redPart = Text.literal("title").formatted(Formatting.RED, Formatting.UNDERLINE);
-                MutableText greenPart = Text.literal("コマンド").formatted(Formatting.GREEN, Formatting.UNDERLINE);
-                MutableText yellowPart = Text.literal("です").formatted(Formatting.YELLOW);
-
-                mainText.append(redPart).append(greenPart).append(yellowPart);
-
-                client.inGameHud.setTitle(mainText);
-
-                // サブタイトルが必要な場合は設定
-                client.inGameHud.setSubtitle(Text.literal("▲こんな風に色々な装飾ができます"));
-            });
-        });
-
-        LOGGER.info("[ModTest] ClientPlayConnectionEvents.JOIN registered.");
+        ClientJoinHandler.register();
 	}
 }
